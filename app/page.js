39bf99a0319c62bf7dee0e51dcd5bd4b6712b80f
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-09-20 21:46:51 +0700

    feat:dateapp

--- app/page.js
+++ app/page.js
@@ -1,113 +1,166 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import PattayaCafe from "public/images/cafePattaya.jpg";
+import BlueSpace from "public/images/BlueSpaceCafe.jpg";
+import Travel from "public/images/travel.jpg";
+import { Roboto, Raleway } from "next/font/google";
 
-export default function Home() {
+const roboto = Raleway({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function DatePlan() {
+  const initialLocations = [
+    {
+      name: "Pattaya",
+      schedule: [
+        {
+          time: "10-12am",
+          activity: "Travel to Pattaya",
+          imageUrl: Travel,
+        },
+        {
+          time: "12-1.30pm",
+          activity: "Good Old Sea Cafe",
+          imageUrl: PattayaCafe,
+          linkUrl: "https://www.facebook.com/goodoldsea/",
+        },
+        {
+          time: "2-3.30pm",
+          activity: "Blue Space Cafe",
+          imageUrl: BlueSpace,
+          linkUrl: "https://www.facebook.com/bluespacecafe/",
+        },
+      ],
+    },
+    {
+      name: "Bangkok",
+      schedule: [
+        // {
+        //   time: "10-11am",
+        //   activity: "Arrive in Bangkok",
+        //   imageUrl: "path_to_bangkok_image1.jpg",
+        // },
+        // {
+        //   time: "11-12pm",
+        //   activity: "Visit Temple",
+        //   imageUrl: "path_to_bangkok_image2.jpg",
+        // },
+        // {
+        //   time: "12-1pm",
+        //   activity: "Lunch at Riverside",
+        //   imageUrl: "path_to_bangkok_image3.jpg",
+        // },
+        // {
+        //   time: "1-2pm",
+        //   activity: "Shopping at MBK",
+        //   imageUrl: "path_to_bangkok_image4.jpg",
+        // },
+      ],
+    },
+  ];
+
+  const [locations, setLocations] = useState(initialLocations);
+  const [selectedLocationName, setSelectedLocationName] = useState(null);
+
+  const handleActivityChange = (event, locationName, entryIndex) => {
+    const newActivity = event.target.value;
+    setLocations((prev) => {
+      const newLocations = [...prev];
+      const locIndex = newLocations.findIndex((l) => l.name === locationName);
+      newLocations[locIndex].schedule[entryIndex].activity = newActivity;
+      return newLocations;
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div
+        className="flex flex-col items-center justify-center min-h-screen px-4 space-y-6 bg-gradient-to-br from-[#f1faee] to-[#f5ebe0] font-serif"
+        style={roboto.style}
+      >
+        <h1 className="text-4xl font-bold text-[#023e8a]">
+          Choose a Date Location
+        </h1>
+
+        {!selectedLocationName ? (
+          <div className="grid grid-cols-1 gap-6 w-full max-w-xl">
+            {locations.map((location, index) => (
+              <button
+                key={index}
+                className="bg-white rounded-lg shadow-md p-4 transition-transform transform hover:scale-105 hover:bg-pink-100"
+                onClick={() => setSelectedLocationName(location.name)}
+              >
+                <h2 className="text-2xl font-semibold text-[#2b2d42]">
+                  {location.name}
+                </h2>
+              </button>
+            ))}
+          </div>
+        ) : (
+          <>
+            <button
+              onClick={() => setSelectedLocationName(null)}
+              className="self-start mb-4 bg-white py-1 px-3 rounded-full shadow-md text-[#2b2d42] hover:bg-pink-100 transition-colors"
+            >
+              ← Go back
+            </button>
+
+            {locations
+              .find((loc) => loc.name === selectedLocationName)
+              .schedule.map((entry, idx) => (
+                <div key={idx} className="mb-4 pb-3">
+                  <div className="flex items-start space-x-4">
+                    <div>
+                      {entry.linkUrl ? (
+                        <a
+                          href={entry.linkUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-decoration-none"
+                        >
+                          <Image
+                            src={entry.imageUrl}
+                            alt={entry.activity}
+                            className="rounded-lg cursor-pointer"
+                            width={250}
+                            height={250}
+                          />
+                        </a>
+                      ) : (
+                        <Image
+                          src={entry.imageUrl}
+                          alt={entry.activity}
+                          className="rounded-lg"
+                          width={250}
+                          height={250}
+                        />
+                      )}
+                    </div>
+                    <div>
+                      <h3 className="text-xl font-semibold mb-2 text-[#2b2d42]">
+                        {entry.time}
+                      </h3>
+                      <input
+                        type="text"
+                        value={entry.activity}
+                        onChange={(e) =>
+                          handleActivityChange(e, selectedLocationName, idx)
+                        }
+                        className="border rounded-md p-2 w-60 focus:border-2 focus:ring-2 focus:ring-indigo-200 shadow-md bg-white placeholder-[#2b2d42]"
+                        placeholder="Enter activity..."
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
